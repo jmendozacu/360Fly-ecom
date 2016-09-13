dba2a4773ae071e5970a64396c96a69a8569fba6
MageMojoServer <mhmserver@magenyc.com>
2016-09-13 20:29:23 +0000

    Riskified 1.0.9.2

---
+++ app/code/community/Riskified/Full/Helper/Data.php
@@ -0,0 +1,107 @@
+<?php
+
+require_once(Mage::getBaseDir('lib') . DIRECTORY_SEPARATOR . 'riskified_php_sdk' . DIRECTORY_SEPARATOR . 'src' . DIRECTORY_SEPARATOR . 'Riskified' . DIRECTORY_SEPARATOR . 'autoloader.php');
+
+class Riskified_Full_Helper_Data extends Mage_Core_Helper_Abstract
+{
+
+    public function getAdminUrl()
+    {
+        $out = null;
+        $match = preg_match("/(.*)full\/response\/getresponse.*/i", Mage::helper('adminhtml')->getUrl('full/response/getresponse'), $out);
+        if ($match) {
+            return $out[1];
+        } else {
+            return "";
+        }
+    }
+
+    public function getAuthToken()
+    {
+        return Mage::getStoreConfig('fullsection/full/key', Mage::app()->getStore());
+    }
+
+    public function getConfigStatusControlActive()
+    {
+        return Mage::getStoreConfig('fullsection/full/order_status_sync');
+    }
+
+    public function getConfigEnv()
+    {
+        return 'Riskified\Common\Env::' . Mage::getStoreConfig('fullsection/full/env');
+    }
+
+    public function getConfigEnableAutoInvoice()
+    {
+        return Mage::getStoreConfig('fullsection/full/auto_invoice_enabled');
+    }
+
+    public function getConfigAutoInvoiceCaptureCase()
+    {
+        return Mage::getStoreConfig('fullsection/full/auto_invoice_capture_case');
+    }
+
+    public function getConfigBeaconUrl()
+    {
+        return Mage::getStoreConfig('fullsection/full/beaconurl');
+    }
+
+    public function getShopDomain()
+    {
+        return Mage::getStoreConfig('fullsection/full/domain');
+    }
+
+    public function getExtensionVersion()
+    {
+        return (string)Mage::getConfig()->getNode()->modules->Riskified_Full->version;
+    }
+
+    public function getDeclinedState()
+    {
+        return Mage::getStoreConfig('fullsection/full/declined_state');
+    }
+
+    public function getDeclinedStatus()
+    {
+        $state = $this->getDeclinedState();
+        return Mage::getStoreConfig('fullsection/full/declined_status_' . $state);
+    }
+
+    public function getApprovedState()
+    {
+        return Mage::getStoreConfig('fullsection/full/approved_state');
+    }
+
+    public function getApprovedStatus()
+    {
+        $state = $this->getApprovedState();
+        return Mage::getStoreConfig('fullsection/full/approved_status_' . $state);
+    }
+
+    public function isDebugLogsEnabled()
+    {
+        return (bool)Mage::getStoreConfig('fullsection/full/debug_logs');
+    }
+
+    public function getSessionId(){
+        //return Mage::getSingleton("core/session")->getEncryptedSessionId();
+        //return Mage::getModel('core/cookie')->get('rCookie');
+        return Mage::getSingleton("core/session")->getSessionId();
+    }
+
+    /**
+     * @return string
+     */
+    public function getSdkVersion()
+    {
+        return Riskified\Common\Riskified::VERSION;
+    }
+
+    /**
+     * @return string
+     */
+    public function getSdkApiVersion()
+    {
+        return Riskified\Common\Riskified::API_VERSION;
+    }
+}
---
+++ app/code/community/Riskified/Full/Model/Authorizenet.php
@@ -0,0 +1,22 @@
+<?php
+
+class Riskified_Full_Model_Authorizenet extends Mage_Paygate_Model_Authorizenet
+{
+    /**
+     * it sets card`s data into additional information of payment model
+     *
+     * @param mage_paygate_model_authorizenet_result $response
+     * @param mage_sales_model_order_payment $payment
+     * @return varien_object
+     */
+    protected function _registerCard(varien_object $response, mage_sales_model_order_payment $payment)
+    {
+        Mage::helper('full/log')->log("in inherited _registercard.");
+        $card = parent::_registercard($response, $payment);
+        $card->setCcAvsResultCode($response->getAvsResultCode());
+        $card->setCcResponseCode($response->getCardCodeResponseCode());
+        $payment->setCcAvsStatus($response->getAvsResultCode());
+        $payment->setCcCidStatus($response->getCardCodeResponseCode());
+        return $card;
+    }
+}
