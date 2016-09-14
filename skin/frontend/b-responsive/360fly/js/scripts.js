7f7e384772af73c1fa6a64da1d33425bf00a34c4
maierb <maierb@users.noreply.github.com>
2016-09-14 18:00:29 -0400

    Fixed Script on community pages, normalize UI, Fixed Bootstrap JS Conflict (#43)

--- skin/frontend/b-responsive/360fly/js/scripts.js
+++ skin/frontend/b-responsive/360fly/js/scripts.js
@@ -1,6 +1,38 @@
 jQuery.noConflict();
+
+function adjustBioLeftWidth(){
+    var wrap_width = jQuery('.wrapper-width').width();
+    var bio_left_width = jQuery('.bio-container .bio-left .bio-left-child-x').width();
+    var bio_left_element = jQuery('.bio-container .bio-left .bio-left-child-x .bio-left-child-container');
+    if(wrap_width == 1000){
+        var bio_left_child_container = ((450 / bio_left_width) * 100)+'%';
+        bio_left_element.css('width', bio_left_child_container);
+        bio_left_element.css('float', 'right');
+        bio_left_element.css('margin-left', '25px');
+    } else{
+        bio_left_element.css('float', 'left');
+        bio_left_element.css('width', '100%');
+        bio_left_element.css('margin-left', 'inherit');
+    }
+}
+
+jQuery(window).resize(function() {
+    adjustBioLeftWidth();
+});
 jQuery(document).ready(function(a) {
-	
+
+    adjustBioLeftWidth();
+
+    /*Community Pages DropDown toggle*/
+    a( "ul li.community-first-dd" ).click(function(e) {
+        a('ul li.community-first-dd ul.community-dropdown').toggle();
+        e.stopPropagation();
+    });
+
+    a(document).click(function(){
+        a("ul li.community-first-dd ul.community-dropdown").hide();
+    });
+
    /*NAVIGATION MENU ANIMATION START*/
     a(".menu1").hover(
         function() {
