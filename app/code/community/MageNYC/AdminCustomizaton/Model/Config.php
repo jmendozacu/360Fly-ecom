18fb32ee65f5a9a4e3482fe26999fa4d0c12aaa5
maierb <maierb@users.noreply.github.com>
2016-09-19 22:38:31 -0400

    Admin Customization Module (#74)

---
+++ app/code/community/MageNYC/AdminCustomizaton/Model/Config.php
@@ -0,0 +1,11 @@
+<?php
+
+abstract class MageNYC_AdminCustomization_Model_Config {
+
+  //XML paths for config values
+  const USECUSTOMHEADER = 'design/magenyc_admincustomization/usecustomheader';
+  const CUSTOMLOGO = 'design/magenyc_admincustomization/logo';
+  const ALTTEXT = 'design/magenyc_admincustomization/alt';
+  const LOGOHEIGHT = 'design/magenyc_admincustomization/logo_height';
+
+}
